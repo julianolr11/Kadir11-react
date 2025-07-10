4ee083a6712f4514b512feeca141cc6d9498ed02
julianolr11 <julianolr11@gmail.com>
2025-07-09 21:28:34 -0300

    olhas

---
+++ frontend/src/components/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from '../../Assets/Logo/kadirnobg.png'
+
+export default function HomeScreen() {
+  return (
+    <div>
+      <h1>Bem-vindo ao Kadir11</h1>
+      <img src={logo} alt="Kadir11" style={{ width: '200px' }} />
+    </div>
+  )
+}
