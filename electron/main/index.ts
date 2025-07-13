e88a21e6ef44264a446bcf0e4c47a6b3b09729fc
julianolr11 <73197713+julianolr11@users.noreply.github.com>
2025-07-13 20:16:47 -0300

    Centralize options close button and hide menu

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -46,12 +46,13 @@ const indexHtml = path.join(RENDERER_DIST, 'index.html')
 
 async function createWindow() {
   win = new BrowserWindow({
-    title: 'Main window',
+    title: 'Kadir11',
     icon: path.join(process.env.VITE_PUBLIC, 'favicon.ico'),
     width: 1536,
     height: 1024,
     resizable: false,
     maximizable: true,
+    autoHideMenuBar: true,
     webPreferences: {
       preload,
       // Warning: Enable nodeIntegration and disable contextIsolation is not secure in production
@@ -62,6 +63,9 @@ async function createWindow() {
       // contextIsolation: false,
     },
   })
+
+  // Ensure the default menu bar is hidden
+  win.setMenuBarVisibility(false)
 
   if (VITE_DEV_SERVER_URL) { // #298
     win.loadURL(VITE_DEV_SERVER_URL)
