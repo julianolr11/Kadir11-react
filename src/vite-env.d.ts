19652a402d41a62ffcefbab452748375d4f17659
julianolr11 <73197713+julianolr11@users.noreply.github.com>
2025-07-21 09:58:18 -0300

    Add tmj map loading API

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,4 +5,7 @@ interface Window {
   ipcRenderer: import('electron').IpcRenderer
   setCursorType?: (type: 'default' | 'pointer' | 'grab' | 'leave') => void
   getPetAssets: (especie: string, elemento: string) => Promise<{ animacaoEvolucao: string, assetPet: string | undefined }>
+  tmjAPI: {
+    loadMap: (mapName: string) => any
+  }
 }
