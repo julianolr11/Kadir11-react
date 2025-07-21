e0405746400ff79f79c9b047e88f5638bc08cfd2
julianolr11 <73197713+julianolr11@users.noreply.github.com>
2025-07-21 11:14:10 -0300

    fix preload path

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -42,7 +42,10 @@ if (!app.requestSingleInstanceLock()) {
 }
 
 let win: BrowserWindow | null = null
-const preload = path.join(__dirname, '../preload.js')
+// Use the project root for the preload script instead of `dist-electron`
+// to avoid issues with missing files when running the application in
+// development.
+const preload = path.join(process.env.APP_ROOT!, 'preload.js')
 const indexHtml = path.join(RENDERER_DIST, 'index.html')
 
 async function createWindow() {
